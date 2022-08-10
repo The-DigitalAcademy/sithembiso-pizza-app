bdde18217ce9e4c6c5334214d4781ccae5733996
sthezahadebe-jpg <ssithembisosifiso@gmail.com>
2022-08-10 09:35:05 +0200

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,14 @@
 import React from "react";
-import InstagramIcon from "@material-ui/icons/Instagram";
-import TwitterIcon from "@material-ui/icons/Twitter";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import { WhatsApp } from "@material-ui/icons";
-// import LinkedInIcon from "@material-ui/icons/LinkedInIcon";
 import "../styles/Footer.css";
 
 function Footer() {
   return (
     <div className="footer">
       <div className="socialMedia">
-        <InstagramIcon />
-        <TwitterIcon /> 
         <FacebookIcon /> 
         <WhatsApp />
-        {/* <LinkedInIcon /> */}
       </div>
       <p> &copy; 2022 ssithembisosifisopizza@gmail.com</p>
     </div>
